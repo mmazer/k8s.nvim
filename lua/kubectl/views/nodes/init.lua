6c20620b7b74e056b680b261456d63a7a6edecf6
Marc Mazer <mmazer@freshbooks.com>
2024-11-18 15:06:18 -0500

    Fix ResourceView constructor call

--- lua/kubectl/views/nodes/init.lua
+++ lua/kubectl/views/nodes/init.lua
@@ -17,12 +17,12 @@ M.view = function()
       local name = lib.current_word()
       local opts = {"-A","-o", "wide", "--field-selector", "spec.nodeName="..name}
       local cmd = kubectl.get("pods", nil, nil, opts)
-      local view = ResourceView:create("pod", cmd, {view_name={"pod", "node="..name}})
+      local view = ResourceView:new("pod", cmd, {view_name={"pod", "node="..name}})
       view:view()
     end
   }
   opts.keymap = keymap
-  local view = ResourceView:create(M.resource, cmd, opts)
+  local view = ResourceView:new(M.resource, cmd, opts)
   view:view()
 end
 
