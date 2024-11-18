5573606584b59823a28aa97e05e059974a657684
Marc Mazer <mmazer@freshbooks.com>
2024-11-18 17:27:36 -0500

    Refactor BufferView for extensibility

--- lua/kubectl/views/bufferview.lua
+++ lua/kubectl/views/bufferview.lua
@@ -7,6 +7,10 @@ function BufferView:new(view_name, cmd, opts)
   instance.view_name = view_name
   instance.cmd = cmd
   instance.opts = opts or {}
+  instance.buffer_options = {
+    buftype = "nofile",
+    swapfile = false
+  }
 
   return instance
 end
@@ -20,24 +24,25 @@ function BufferView:open()
   if opts.namespace then
     self:set_view_namespace(opts.namespace)
   end
-  vim.api.nvim_cmd({cmd="enew"}, {})
-  if opts.namespace then
-    self:set_view_namespace(opts.namespace)
-  end
-  local buf = vim.api.nvim_get_current_buf()
-  vim.api.nvim_set_option_value("buftype", "nofile", { buf = buf })
-  vim.api.nvim_set_option_value("swapfile", false, { buf = buf })
+
+  local buf = self:create_buffer()
+
+  self:set_buffer_options(self.opts.buffer)
+
   vim.api.nvim_buf_set_name(buf, "Kubectl:"..table.concat(self.view_name, " "))
   vim.api.nvim_buf_set_lines(buf, 0, -1, true, vim.split(data, "\n", {plain=true}))
-  vim.opt_local.iskeyword:append("-")
-  vim.opt_local.iskeyword:append(".")
-  vim.opt_local.iskeyword:append(":")
+
+  local keywords = self:get_buffer_keywords()
+  for _,v in pairs(keywords) do
+    vim.opt_local.iskeyword:append(v)
+  end
 
   if opts.filetype ~= nil then
     vim.api.nvim_set_option_value("filetype", opts.filetype, { buf = buf })
   end
 
   vim.api.nvim_win_set_cursor(0, {1,0})
+
   if opts.keymap ~= nil then
     for k,v in pairs(opts.keymap) do
     vim.keymap.set({"n"}, k, v, {buffer=buf, silent=true})
@@ -52,6 +57,30 @@ function BufferView:open()
   vim.keymap.set({"n"}, "gr", function()
     view:refresh()
   end, {buffer=buf})
+end
+
+function BufferView:set_buffer_options(buffer, opts)
+  local buffer_options = {}
+  if self.buffer_options ~= nil then
+    lib.table_extend(buffer_options, self.buffer_options)
+  end
+  if opts ~= nil then
+    lib.table_extend(buffer_options, opts)
+  end
+  for k,v in pairs(buffer_options) do
+    vim.api.nvim_set_option_value(k, v, { buf = buffer})
+  end
+  return buffer_options
+end
+
+function BufferView:get_buffer_keywords()
+  local keywords = {"-", ":", "."}
+  return keywords
+end
+
+function BufferView:create_buffer()
+  vim.api.nvim_cmd({cmd="enew"}, {})
+  return vim.api.nvim_get_current_buf()
 end
 
 function BufferView:refresh()
