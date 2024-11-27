d5bf58775f2f658cbd53c0d2a62349dfeca12cd9
Marc Mazer <mmazer@freshbooks.com>
2024-11-27 15:12:57 -0500

    Use resource specs

--- lua/kubectl/init.lua
+++ lua/kubectl/init.lua
@@ -1,29 +1,14 @@
-local resource_aliases = require("kubectl.views.types").resource_aliases
-local customresources = require("kubectl.views.customresources")
+local resourcespecs = require("kubectl.views.resourcespecs")
 local M = {}
-
-local get_resource_for_alias = function(alias)
-  local resource = resource_aliases[alias]
-  if resource == nil or resource == '' then
-    resource = alias
-  end
-
-  return resource
-
-end
 
 M.setup = function(options)
   options = options or {}
-  local custom_resources = options.custom_resources
-  if custom_resources ~= nil then
-    for _,cr in ipairs(custom_resources) do
-      customresources.register(cr)
-    end
-  end
+  resourcespecs.init(options.custom_resources)
 
   vim.api.nvim_create_user_command("Kubectl", function(opts)
     if #opts.fargs == 0 then
-      require("kubectl.views.pods").view()
+      local view = resourcespecs.get_resource_view("pods")
+      view()
       return
     end
 
@@ -61,22 +46,16 @@ M.setup = function(options)
 
     if action == "get" then
       if #opts.fargs >= 2 then
-        local resource_type = get_resource_for_alias(opts.fargs[2])
-        -- check for customresource
-        local customresource = customresources.get_customresource(resource_type)
-        if customresource ~= nil then
-            customresource()
-            return
+        local resource = opts.fargs[2]
+        local view = resourcespecs.get_resource_view(resource)
+        if view ~= nil then
+          view()
+          return
         end
 
-        local ok, view = pcall(require, "kubectl.views." .. resource_type)
-        if ok then
-          view.view()
-        else
-          vim.notify("no view found for "..resource_type..": running user command", vim.log.levels.INFO)
-          require("kubectl.views.user").view(opts.fargs)
+        vim.notify("no view found for "..resource..": running user command", vim.log.levels.INFO)
+        require("kubectl.views.user").view(opts.fargs)
         end
-      end
     else
       require("kubectl.views.user").view(opts.fargs)
     end
